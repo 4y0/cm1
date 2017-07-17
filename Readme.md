53896a0d159e54d06a8ea33ca22780bba3edc1b3
4y0 <4y0d3jii@gmail.com>
2017-07-17 06:00:03 +0100

    Init

---
+++ Readme.md
@@ -0,0 +1,106 @@
+## Command Memorizer [V1]
+cm1 is a simple CLI tool for managing frequently run commands. It can be used to memorize chain of CLI commands, create short key aliases for easy recall and run processes at a later time.
+
+Cm1 also provides a simple web GUI for managing commands (https://cm1cs.herokuapp.com/) .. This is particularly useful if you use / run same commands on different devices or if you are changing devices altogether. You can manage these commands from the web gui, pull them into your local device using the token generated from the GUI.
+
+### Installation
+```
+npm install -g cm1
+```
+
+
+### Memorize a command
+```
+cm1 --add "git add --all && git commit -am {optional_placeholder}" basic-commit
+```
+Memorize the chained commands "git add --all && git commit -am {optional_placeholder}" and index with the key `basic-commit`
+
+
+
+### Recall and run memorized command
+```
+cm1 basic-commit "Init cm1 repository"
+```
+Run the command[s] indexed with `basic-commit` passing "Init cm1 repository" as a replacement for any placeholders in the indexed command:
+`git add --all && git commit -am "Init cm1 repository"`
+
+
+
+### Recall command but don't run
+```
+cm1 basic-commit "Init cm1 repository" --noexec
+```
+Just echo command on the CLI:
+`git add --all && git commit -am "Init cm1 repository"`
+
+
+
+### List memorized commands
+```
+cm1 --list
+```
+Key:  gc  Command:  git commit -am {message} && git push origin {branch}  To run: cm1  gc
+Key:  gr  Command:  sudo rm -fr .git  To run: cm1  gr
+
+
+
+### Rename command key
+```
+cm1 -r basic-commit bc
+```
+Running `cm1 bc "some commit message"` will now do `git add --all && git commit -am "some commit message"`
+
+
+### Modify command
+```
+cm1 -m bc "git add --all && git commit -am {commitmessage} && git push origin {branchname}"
+```
+
+
+### Remove a memorized command
+```
+cm1 --clear bc
+```
+
+
+### Clear all memorized commands
+```
+cm1 --clear
+```
+
+
+### Set cm1 token
+```
+cm1 --init tokenvalue [-d]
+```
+Token can be gotten from (https://cm1cs.herokuapp.com/). If the -d flag is passed, tokenvalue will also be set as the default token to use with cm1.
+
+
+
+
+### Set cm1 token as default
+```
+cm1 --default tokenvalue
+```
+
+
+
+### Push locally stored commands to cloud storage
+```
+cm1 --push
+```
+
+
+
+
+### Pull commands stored in cloud storage to local storage
+```
+cm1 --pull [-f]
+```
+If -f flag is passed, pulled commands will overwrite existing commands in local storage else, a merge is made.
+
+
+
+
+
+
---
+++ cm1.js
@@ -0,0 +1,291 @@
+#!/usr/bin/env node
+var program = require('commander'); 
+var nc = require('node-cmd');
+var _ = require('underscore');
+var commandAlias;
+var storage = require('./lib/storage');
+var cloud_storage = require('./lib/cloud.storage');
+var getStorage = storage.getStorage, updateStorage = storage.updateStorage;
+ 
+
+/*
+Coercion essentially means, 
+taking argument in <vnmn> and replacing program.command with it
+so --add <vnmn> will mean program.add has value passed as vnmn
+*/
+
+program
+  .version('0.0.0')
+  .option('-a, --add <vnmn>', 'Add a command to remember with an optional key to fetch it')
+  .option('-i, --init <token>', 'Set User token to store stuff for')
+  .option('-d, --default', 'Set token as default')
+  .option('-l, --list', 'List all stored commands')
+  .option('-c, --clear', 'Clear a specific command or all commands')
+  .option('-r, --rename <oldcommand>', 'Change command key')
+  .option('-m, --modify <commandkey>', 'Change command assoc.d with a key')
+  .option('-t, --tokenalias <existingtoken>', 'Set alias for 32 bit token key')
+  .option('-x, --noexec', 'List all stored commands')
+  .option('--pull', 'Pull stored commands from cloud storage')
+  .option('--push', 'Push stored commands to cloud storage')
+  .option('-f,--force', 'Override local storage with cloud storage. Used with --pull')
+  .parse(process.argv);
+
+
+
+if(program.init){
+
+	var token = program.init; 
+	console.log(token);
+	if(typeof token == "undefined"){
+		console.log("Please specify a token");
+	}else{
+	var localStorage = getStorage();
+	var old_user_token = localStorage.user_token;
+	localStorage.user_token = token;
+
+	if(localStorage.local[old_user_token]){
+		var tmp = localStorage.local[old_user_token];
+		localStorage.local[token] = tmp;
+		delete localStorage.local[old_user_token];
+	}
+
+	if(program.default){
+		localStorage.default_token = token;
+	}
+	updateStorage(localStorage);
+	}
+
+}
+
+
+if(program.default && !program.init){
+
+	var token = program.args[0]; 
+	console.log(token);
+	if(typeof token == "undefined"){
+		console.log("Please specify a token");
+	}else{
+	var localStorage = getStorage();  
+	localStorage.default_token = token; 
+	updateStorage(localStorage);
+	}
+
+}
+
+
+if(program.tokenalias){
+
+	var token = program.tokenalias; 
+	var alias = program.args[0];
+	console.log(token);
+	if(typeof token == "undefined"){
+		console.log("Please specify a token");
+	}else if(alias){
+
+	var localStorage = getStorage();  
+	localStorage.token_aliases[alias] = token;
+	 
+	updateStorage(localStorage);
+	}
+
+}
+
+
+//Store a new command
+if(program.add){
+
+	var localStorage = getStorage();
+	var file_rem_key = program.args[0]; 
+	var token = localStorage.default_token;
+	if(!localStorage.local[token]){
+		localStorage.local[token] = {commands:{}};
+	}
+	if(file_rem_key){
+		localStorage.local[token].commands[file_rem_key] = program.add;
+	}
+
+	//persist to file
+	updateStorage(localStorage);
+	console.log("Command added")
+
+}
+
+
+if(program.pull){
+
+	var localStorage = getStorage();
+
+	cloud_storage
+	.pull(localStorage.default_token)
+	.then( function (r) {
+			
+		if(r){
+
+			if(!localStorage.local[localStorage.default_token])
+				localStorage.local[localStorage.default_token] = {commands:{}};
+
+			if(program.force){
+				console.log("Force");
+				localStorage.local[localStorage.default_token].commands = r;
+			}
+			else{
+				localStorage.local[localStorage.default_token].commands = _.extend(localStorage.local[localStorage.default_token].commands, r);
+			}
+			updateStorage(localStorage);
+		}
+
+	})
+	.catch( function (e) {
+
+		console.log("Some error occured", e.message);
+
+	});
+
+}
+
+if(program.push){
+	
+	var localStorage = getStorage();
+	if(!localStorage.local[localStorage.default_token])
+		localStorage.local[localStorage.default_token] = {commands:{}};
+
+	cloud_storage
+	.push(localStorage.default_token, localStorage.local[localStorage.default_token].commands)
+	.then( function (r) {
+
+		console.log("Commands successfully pushed to cloud");
+	})
+	.catch( function (e) {
+
+		console.log("Some error occured", e.message);
+	})
+
+
+}
+
+
+//List all stored commands
+if(program.list){
+
+	var localStorage = getStorage();
+	var token = localStorage.default_token;
+
+	if(localStorage.local[token] && localStorage.local[token].commands){
+		var commands = localStorage.local[token].commands
+		for(item in commands){
+			console.log('Key: ', item, ' Command: ', commands[item], ' To run: cm1 ', item);
+		}
+	}else{
+		console.log("No commands to list for token", token);
+	}
+
+}
+
+
+if(program.modify){
+
+	var localStorage = getStorage();
+	var token = localStorage.default_token;
+	var new_command = program.args[0];
+	var command_key = program.modify;
+	if(localStorage.local[token] && localStorage.local[token].commands){
+
+		if(localStorage.local[token].commands[command_key] && new_command){
+
+			localStorage.local[token].commands[command_key] = new_command; 
+
+		}
+	}
+	updateStorage(localStorage);
+}
+
+if(program.rename){
+
+	var localStorage = getStorage();
+	var token = localStorage.default_token;
+	var new_command_key = program.args[0];
+	var old_command_key = program.rename;
+	if(localStorage.local[token] && localStorage.local[token].commands){
+
+		if(new_command_key && localStorage.local[token].commands[old_command_key]){
+
+			var old = localStorage.local[token].commands[old_command_key];
+			localStorage.local[token].commands[new_command_key] = old;
+			delete localStorage.local[token].commands[old_command_key];
+			//old = null;
+
+		}
+	}
+	updateStorage(localStorage);
+}
+
+if(program.clear){
+
+	var localStorage = getStorage();
+	var token = localStorage.default_token;
+	var command_to_clear = program.args[0];
+	if(localStorage.local[token] && localStorage.local[token].commands){
+
+		if(command_to_clear && localStorage.local[token].commands[command_to_clear]){
+			delete localStorage.local[token].commands[command_to_clear];
+		}else{
+			localStorage.local[token].commands = {};
+		}
+	}
+	updateStorage(localStorage);
+}
+	
+function processReplacements(command, args){
+
+	var subst_regex = /(\{[a-z0-9]+\})/ig;
+	var placeholders = command.match(subst_regex);
+
+	if(args.length > 1 && placeholders){
+
+		placeholders
+		.forEach(
+
+			function (p, i){
+				if(args[i + 1]){
+					var replacement = args[i + 1];
+					if(/\s/.test(replacement)){
+						replacement = '"' + replacement + '"';
+					}
+					command = command.replace(p, replacement);	
+				}
+				
+			}
+		)
+
+	}
+	return command;
+}
+
+//get single command
+if(!program.pull && !program.push && !program.list && !program.clear && !program.add && !program.init && !program.default && !program.rename && !program.modify && !program.tokenalias){
+
+	//console.log(program.args);
+	var file_rem_key = program.args[0];
+	if(file_rem_key && getStorage()[file_rem_key.trim()] ){
+
+		var command_to_recall = getStorage()[file_rem_key]; 
+
+		command_to_recall = (processReplacements(command_to_recall, program.args));
+		if(program.noexec){
+			console.log(command_to_recall);
+		}
+		else{
+			//nc.run(command_to_recall);
+			console.log("==== Running command: ", command_to_recall, " ====");
+			nc.get(command_to_recall, function (err, data, stderr){
+				console.log(data);
+			});
+		}
+	}
+	else{
+		console.log('No MEMR entry with the key', file_rem_key);
+	}
+
+}
+
+//memr -a "git log admin" logadmin
